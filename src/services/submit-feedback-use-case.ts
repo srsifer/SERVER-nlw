8cf3e710b08487e468b5f6307d8ef58981187f65
igor s fernandes <igorsife80@gmail.com>
2022-05-05 14:14:04 -0300

    feat: cria a classe que fara a chamada do serviço do envio de email e contem as regras de negocio

---
+++ src/services/submit-feedback-use-case.ts
@@ -0,0 +1,47 @@
+import { MailAdapter } from "../adapters/mail-adapters";
+import { FeedBacksRepository } from "../repositories/feedbacks-repositories";
+
+interface SubimitFeedbackUseCaseRequest {
+    type: string,
+    comment: string,
+    screenshot?: string,
+}
+
+export class SubimitFeedbackUseCase {
+    constructor(
+        private feedbackRepository: FeedBacksRepository,
+        private mailAdapter: MailAdapter,
+    ){}
+
+    async execute(request: SubimitFeedbackUseCaseRequest) {
+        const {type, comment, screenshot} = request;
+
+        if(!type) {
+            throw new Error('Type is required');
+        }
+
+        if (!comment) {
+            throw new Error('Comment is required');
+        }
+
+        if(screenshot && !screenshot.startsWith('data:image/png;base64')) {
+            throw new Error('Invalid screenshot format.')
+        }
+
+         await this.feedbackRepository.create({
+             type,
+             comment,
+             screenshot,
+         })
+         
+         await this.mailAdapter.sendMail({
+            subject: 'novo feedback meu app nlw',
+            body: [
+                `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
+                `<p>Tipo do feedback: ${type}</p>`,
+                `<p>Comentário do feedback: ${comment}</p>`,
+                `</div>`
+            ].join('\n')
+         })
+    }
+}
